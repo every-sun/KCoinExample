57fda12ba3b1c6fabdf00e81c0513ebf8375a15c
jodahye <everysun@amuz.co.kr>
2025-02-25 15:32:19 +0900

    Menu 컴포넌트 수정

--- resources/js/Components/Menus.vue
+++ resources/js/Components/Menus.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="text-white flex flex-col pt-5 gap-1">
         <Link
-            :href="item.url"
+            :href="item.url[0]"
             :class="[
                 'text-white py-4 px-3 text-sm cursor-pointer',
-                url === item.url && 'bg-secondary rounded-md',
+                item.url.includes(url) && 'bg-secondary rounded-md',
             ]"
             v-for="item in menus"
             :key="item.label"
@@ -22,15 +22,23 @@ const url = window.location.href.split("?page=")[0];
 const route = inject("route");
 
 const menus = [
-    { label: "K-Coin 사용", role: "user", url: route("user.coin.use.index") },
+    { label: "K-Coin 사용", role: "user", url: [route("user.coin.use.index")] },
     {
         label: "K-Coin 관리",
         role: "user",
-        url: route("user.coin.manage.index"),
+        url: [route("user.coin.manage.index")],
     },
-    { label: "문의 게시판", role: "user", url: route("inquiry.index") },
-    { label: "공지사항", role: "user", url: route("announcement.index") },
-    { label: "상품 관리", role: "admin", url: route("admin.product.index") },
-    { label: "K-Coin 관리", role: "admin", url: route("admin.coin.index") },
+    { label: "문의 게시판", role: "user", url: [route("inquiry.index")] },
+    {
+        label: "공지사항",
+        role: "user",
+        url: [route("announcement.index"), route("admin.announcement.create")],
+    },
+    {
+        label: "상품 관리",
+        role: "admin",
+        url: [route("admin.product.index"), route("admin.product.create")],
+    },
+    { label: "K-Coin 관리", role: "admin", url: [route("admin.coin.index")] },
 ];
 </script>
